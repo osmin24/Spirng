8d55d4d3199fcd4eb2604076995b148e396bf165
osminPeña <osminv91@gamil.com>
2024-09-13 09:40:27 -0500

    Get file Clientes, Empleados, Flores and Propietario

--- src/main/java/com/example/demo/repository/Flores.java
+++ src/main/java/com/example/demo/repository/Flores.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
 
-public class pelicula {
+public class Flores {
     
 }
--- src/main/java/com/example/demo/services/Clientes.java
+++ src/main/java/com/example/demo/services/Clientes.java
@@ -1,5 +1,5 @@
 package com.example.demo.services;
 
-public class pelicula {
+public class Clientes {
     
 }
