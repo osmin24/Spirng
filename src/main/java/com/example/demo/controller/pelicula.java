c6aa4556bc843c98aef3478d836f38071ebd981b
osminPeña <osminv91@gamil.com>
2024-09-12 21:02:00 -0500

    Primer comit0

---
+++ src/main/java/com/example/demo/controller/pelicula.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/peliculas")
+public class pelicula {
+    
+    @GetMapping("/list")
+    public String listPeliculas(){
+        return "pelicula";
+    }
+}
